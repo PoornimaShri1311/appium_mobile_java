3b752b11f511f9cd2073d5c8e47838edd63d4d8e
PoornimaShri <shriga1990@gmail.com>
2025-05-15 20:48:03 +0200

    Updated PasscodeTest file

--- src/test/java/tests/PasscodeTest.java
+++ src/test/java/tests/PasscodeTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.BiometricConfirmationPage;
 import pages.NotificationPage;
@@ -9,7 +10,7 @@ import pages.TrustWalletLaunchPage;
 
 public class PasscodeTest extends BaseTest {
 
-    @Test(description = "Validate Create Wallet Launch")
+    //    @Test(description = "Validate Create Wallet Launch")
     public void trustWalletTest() {
         try {
             test.info("Starting test: trustWalletTest");
@@ -18,25 +19,29 @@ public class PasscodeTest extends BaseTest {
             test.pass("Clicked on Create New Wallet successfully");
         } catch (Exception e) {
             test.fail("Test failed due to exception: " + e.getMessage());
-            // Optionally adding stack trace details
             test.fail(e);
-            throw e;  // re-throw to mark the test as failed in TestNG
+            throw e;
         }
     }
 
-    @Test(description = "Validate Create Wallet Passcode")
-    public void createPasscode() throws InterruptedException {
+    @Test(dataProvider = "passcodeData", description = "Validate Create Wallet Passcode")
+    public void createPasscode(String passcode) throws InterruptedException {
         try {
-            test.info("Starting test: createPasscode");
+            TrustWalletLaunchPage trustWalletLaunchPage = new TrustWalletLaunchPage();
+            trustWalletLaunchPage.clickCreateNewWalletButton();
+            test.pass("Clicked on Create New Wallet successfully");
+            test.info("Starting test: createPasscode with passcode: " + passcode);
             PasscodePage passcodePage = new PasscodePage();
-            passcodePage.enterPasscode("123456");
-            passcodePage.enterPasscode("123456");
+            passcodePage.enterPasscode(passcode);
+            passcodePage.enterPasscode(passcode);
             test.pass("Passcode entered successfully");
+
             test.info("Starting test: biometricConfirmationTest");
             BiometricConfirmationPage biometricConfirmationPage = new BiometricConfirmationPage();
             Assert.assertTrue(biometricConfirmationPage.isBiometricLoginDisplayed(), "Biometric Login text not displayed!");
             biometricConfirmationPage.tapOnDenyButton();
             test.pass("Deny button clicked successfully");
+
             test.info("Starting test: notificationTest");
             NotificationPage notificationPage = new NotificationPage();
             Assert.assertEquals(notificationPage.getKeepUpWithMarketText(), "Keep up with the market!", "Market text does not match!");
@@ -45,9 +50,15 @@ public class PasscodeTest extends BaseTest {
             test.pass("Skip Later button clicked successfully");
         } catch (Exception e) {
             test.fail("Test failed due to exception: " + e.getMessage());
-            // Optionally adding stack trace details
             test.fail(e);
-            throw e;  // re-throw to mark the test as failed in TestNG
+            throw e;
         }
+    }
+
+    @DataProvider(name = "passcodeData")
+    public Object[][] passcodeData() {
+        return new Object[][]{
+                {"123456"}
+        };
     }
 }
